f21346514e1bffdb6af06ea70b638fd6333e8b8d
jagan9691 <jagan9691@gmail.com>
2022-08-15 10:55:41 +0530

    msg

--- src/DAY1.java
+++ src/DAY1.java
@@ -37,6 +37,10 @@ public class DAY1 {
 		// TODO Auto-generated method stub
 
 	}
+	private void meth11() {
+		// TODO Auto-generated method stub
+
+	}
 
 	public static void main(String[] args) {
 
