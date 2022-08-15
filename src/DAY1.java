2f4c7a7bcebdb31642181efaeaa66f332f0f6ed2
jagan9691 <jagan9691@gmail.com>
2022-08-15 10:02:48 +0530

    new

---
+++ src/DAY1.java
@@ -0,0 +1,29 @@
+
+public class DAY1 {
+
+
+		private void meth1() {
+			System.out.println("1");
+		}
+
+		private void meth2() {
+			System.out.println("2");
+		}
+
+		private void meth3() {
+			System.out.println("3");
+
+		}
+		private void meth4() {
+			// TODO Auto-generated method stub
+
+		}
+
+		public static void main(String[] args) {
+
+			DAY1 r = new DAY1();
+
+		}
+	}
+
+
